48ac5bf77f5f7aeeabad2f1ebfa4e6faaea487a5
Anton <antonymousfeedback@gmail.com>
2023-07-12 14:39:39 +0200

    Refactor, add access checks

--- src/base-request-handler.js
+++ src/base-request-handler.js
@@ -1,20 +1,19 @@
 import { getClient } from './get-client.js'
 
 function baseRequestHandler (requestFunction, context) {
-  const { services } = context
+  const {services} = context
   return async function (req, res, next) {
     if (!req.accountability?.user) {
       console.log('Keycloak: Anonymous rejected')
       res.status(401)
-      return res.send({ message: 'api_errors.unauthorized' })
+      return res.send({message: 'api_errors.unauthorized'})
     }
-    const { UsersService } = services
-    const usersService = new UsersService({ schema: req.schema, accountability: req.accountability })
+    const {UsersService} = services
+    const usersService = new UsersService({schema: req.schema, accountability: req.accountability})
     let user
     try {
       user = await usersService.readOne(req.accountability.user)
-    }
-    catch (err) {
+    } catch (err) {
       console.error('Keycloak: Failed to get current user', req.accountability.user, err.message)
     }
     const client = await getClient()
@@ -23,8 +22,8 @@ function baseRequestHandler (requestFunction, context) {
         let userGroups = [], isAllowed = false
         if (user?.external_identifier) {
           console.log('Keycloak: Fetch external user id', user.external_identifier);
-          ({ data: userGroups } = await client.get(`/users/${user?.external_identifier}/groups`));
-          isAllowed = !!userGroups.find(group => group.name === 'management');
+          ({data: userGroups} = await client.get(`/users/${user?.external_identifier}/groups`))
+          isAllowed = !!userGroups.find(group => ['staff', 'management'].includes(group.name))
         }
         if (req.accountability.admin) {
           console.log('Keycloak: Admin user')
@@ -32,23 +31,21 @@ function baseRequestHandler (requestFunction, context) {
         }
         if (!isAllowed) {
           res.status(403)
-          return res.send({ message: 'api_errors.forbidden' })
+          return res.send({message: 'api_errors.forbidden'})
         }
 
-        const result = await requestFunction({ req, res, next, client, user, userGroups })
+        const result = await requestFunction({req, res, next, client, user, userGroups})
         if (result) return res.send(result)
-      }
-      catch (err) {
+      } catch (err) {
         if (err.response) {
           res.status(err.response.status)
           return res.send(err.response.data)
         }
         next(err)
       }
-    }
-    else {
+    } else {
       res.status(503)
-      return res.send({ message: 'api_errors.service_unavailable' })
+      return res.send({message: 'api_errors.service_unavailable'})
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,49 @@ import { filterUser } from './filter-props.js'
 import { generatePassword } from './random-password.js'
 
 const OTP_LENGTH = 6
-const logPrefix = 'Keycloak Endpoint: '
+const LOG_PREFIX = 'Keycloak Endpoint: '
+const TYPES = ['student', 'staff', 'management', 'admin']
+
+async function isRequestAllowed (ctx) {
+  const {client, req, userGroups} = ctx
+  let subjectTypeGroup, subjectGroups
+  if (req.params.id) ({data: subjectGroups} = await client.get(`/users/${req.params.id}/groups`))
+
+  if (req.method === 'POST') {
+    subjectTypeGroup = {name: req.body.type}
+  } else if (req.method === 'PATCH') {
+    if (req.body.type) subjectTypeGroup = {name: req.body.type}
+    else subjectTypeGroup = subjectGroups.find(group => TYPES.includes(group.name))
+  } else {
+    subjectTypeGroup = subjectGroups.find(group => TYPES.includes(group.name))
+  }
+
+  const userType = userGroups.find(group => TYPES.includes(group.name))
+  const userTypeIndex = TYPES.indexOf(userType ? userType.name : 'admin')
+  const subjectTypeIndex = TYPES.indexOf(subjectTypeGroup.name)
+
+  if (userTypeIndex < subjectTypeIndex) throw new Error('forbidden')
+}
+
+function handleErrorResponse (ctx, err) {
+  const {res} = ctx
+  if (err.message === 'validation_failed') {
+    res.status(400)
+    res.send({message: 'api_errors.validation_failed'})
+  } else if (err.message === 'forbidden') {
+    res.status(403)
+    res.send({message: 'api_errors.forbidden'})
+  } else {
+    res.status(500)
+    res.send({message: err.message})
+  }
+}
 
 export default {
   id: 'keycloak',
   handler: (router, context) => {
-    const { services, logger } = context
-    const { ItemsService, UsersService } = services
+    const {services, logger} = context
+    const {ItemsService, UsersService} = services
     //
     // Users
     //
@@ -19,25 +55,40 @@ export default {
     //
     // FIND
     router.get('/users', baseRequestHandler(async ctx => {
-      const { data: users } = await ctx.client.get('/users', { params: ctx.req.query })
+      const {data: users} = await ctx.client.get('/users', {params: ctx.req.query})
       return users.map(user => filterUser(user))
+    }, context))
+
+    //
+    // GET
+    router.get('/users/:id', baseRequestHandler(async ctx => {
+      const {client, req} = ctx
+      const {data: user} = await client.get(`/users/${req.params.id}`)
+      const {data: groups} = await client.get(`/users/${req.params.id}/groups`)
+
+      const association = groups.find(group => enumAssociation.includes(group.name))
+      if (association) user.association = association.name
+      const type = groups.find(group => enumType.includes(group.name))
+      if (association) user.type = type.name
+
+      user.profiles = groups.filter(group => group.name.indexOf('gpu-') === 0).map(group => group.name)
+
+      return filterUser(user)
     }, context))
 
     //
     // POST
     router.post('/users', baseRequestHandler(async ctx => {
-      const data = ctx.req.body
-      const createDirectusUser = !data.skipDirectusUser
       try {
-        validateUser(data, 'post')
+        validateUser(ctx.req.body, 'post')
+        await isRequestAllowed(ctx)
+      } catch (err) {
+        return handleErrorResponse(ctx, err)
       }
-      catch (err) {
-        ctx.res.status(400)
-        return ctx.res.send({
-          message: 'api_errors.validation_failed',
-          errorMessage: err.message
-        })
-      }
+
+      const {client, req, res, userGroups} = ctx
+      const data = req.body
+      const createDirectusUser = !data.skipDirectusUser
 
       const associationName = data.association
       delete data.association
@@ -45,7 +96,7 @@ export default {
       delete data.type
       data.enabled = true
 
-      const { data: groups } = await ctx.client.get('/groups')
+      const {data: groups} = await client.get('/groups')
 
       let profiles = []
       if (Array.isArray(data.profiles)) {
@@ -53,22 +104,22 @@ export default {
         delete data.profiles
       }
 
-      const response = await ctx.client.post('/users', data)
+      const response = await client.post('/users', data)
       const location = new URL(response.headers.location)
       const userId = basename(location.pathname)
-      logger.info(`${logPrefix}Created Keycloak user with ID ${userId}`)
+      logger.info(`${LOG_PREFIX}Created Keycloak user with ID ${userId}`)
 
       const association = groups.find(group => group.name === associationName)
-      if (association) await ctx.client.put(`/users/${userId}/groups/${association.id}`)
+      if (association) await client.put(`/users/${userId}/groups/${association.id}`)
       const type = groups.find(group => group.name === typeName)
-      if (type) await ctx.client.put(`/users/${userId}/groups/${type.id}`)
+      if (type) await client.put(`/users/${userId}/groups/${type.id}`)
 
       for (const profile of profiles) {
-        await ctx.client.put(`/users/${userId}/groups/${profile.id}`)
+        await client.put(`/users/${userId}/groups/${profile.id}`)
       }
 
       const pwd = generatePassword(OTP_LENGTH)
-      await ctx.client.put(`/users/${userId}/reset-password`, {
+      await client.put(`/users/${userId}/reset-password`, {
         value: pwd,
         temporary: true
       })
@@ -82,20 +133,22 @@ export default {
           email: data.email,
           external_identifier: userId
         }
-        const mappingService = new ItemsService('role_group_mapping', { schema: ctx.req.schema, accountability: ctx.req.accountability })
-        const usersService = new UsersService({ schema: ctx.req.schema, accountability: ctx.req.accountability })
+        const mappingService = new ItemsService('role_group_mapping', {
+          schema: req.schema,
+          accountability: req.accountability
+        })
+        const usersService = new UsersService({schema: req.schema, accountability: req.accountability})
         try {
           const groupId = `${association.name}-${type.name === 'management' ? 'staff' : type.name}`
-          const results = await mappingService.readByQuery({ filter: { groupId } })
-          const roleId = results.map(mapping => mapping.roleId).shift();
+          const results = await mappingService.readByQuery({filter: {groupId}})
+          const roleId = results.map(mapping => mapping.roleId).shift()
           if (roleId) {
             userPayload.role = roleId
             directusUserId = await usersService.createOne(userPayload)
-            logger.info(`${logPrefix}Created Directus user with ID ${directusUserId}`)
-          }
-          else throw new Error('No matching role ID found')
+            logger.info(`${LOG_PREFIX}Created Directus user with ID ${directusUserId}`)
+          } else throw new Error('No matching role ID found')
         } catch (err) {
-          logger.error(`${logPrefix}Failed to create Directus user: ${err.message}`)
+          logger.error(`${LOG_PREFIX}Failed to create Directus user: ${err.message}`)
         }
       }
 
@@ -107,40 +160,23 @@ export default {
     }, context))
 
     //
-    // GET
-    router.get('/users/:id', baseRequestHandler(async ctx => {
-      const { data: user } = await ctx.client.get(`/users/${ctx.req.params.id}`)
-      const { data: groups } = await ctx.client.get(`/users/${ctx.req.params.id}/groups`)
-
-      const association = groups.find(group => enumAssociation.includes(group.name))
-      if (association) user.association = association.name
-      const type = groups.find(group => enumType.includes(group.name))
-      if (association) user.type = type.name
-
-      user.profiles = groups.filter(group => group.name.indexOf('gpu-') === 0).map(group => group.name)
-
-      return filterUser(user)
-    }, context))
-
-    //
     // PATCH
     router.patch('/users/:id', baseRequestHandler(async ctx => {
-      const { client, req } = ctx
-      const data = req.body
       try {
-        validateUser(data, 'patch')
+        validateUser(ctx.req.body, 'patch')
+        await isRequestAllowed(ctx)
+      } catch (err) {
+        return handleErrorResponse(ctx, err)
       }
-      catch (err) {
-        ctx.res.status(400)
-        return ctx.res.send({
-          message: 'api_errors.validation_failed',
-          errorMessage: err.message
-        })
-      }
-      const { data: groups } = await client.get(`/groups`)
-      const { data: userGroups } = await client.get(`/users/${req.params.id}/groups`)
+
+      const {client, req, res, userGroups} = ctx
+      const data = req.body
+
+      const {data: groups} = await client.get(`/groups`)
+      const {data: subjectGroups} = await client.get(`/users/${req.params.id}/groups`)
+
       if (data.association) {
-        const existingAssociation = userGroups.find(group => enumAssociation.includes(group.name))
+        const existingAssociation = subjectGroups.find(group => enumAssociation.includes(group.name))
         const association = groups.find(group => group.name === data.association)
         if (data.association !== existingAssociation?.name) {
           if (existingAssociation) {
@@ -151,7 +187,7 @@ export default {
         delete data.association
       }
       if (data.type) {
-        const existingType = userGroups.find(group => enumType.includes(group.name))
+        const existingType = subjectGroups.find(group => enumType.includes(group.name))
         const type = groups.find(group => group.name === data.type)
         if (data.type !== existingType?.name) {
           if (existingType) {
@@ -162,7 +198,7 @@ export default {
         delete data.type
       }
       if (data.profiles) {
-        const existingProfiles = userGroups.filter(group => profilePattern.test(group.name))
+        const existingProfiles = subjectGroups.filter(group => profilePattern.test(group.name))
         for (const profileName of data.profiles) {
           if (!existingProfiles.find(p => p.name === profileName)) {
             const profile = groups.find(group => group.name === profileName)
@@ -176,33 +212,40 @@ export default {
         }
         delete data.profiles
       }
-      const { data: result } = await client.put(`/users/${req.params.id}`, data)
+      const {data: result} = await client.put(`/users/${req.params.id}`, data)
       return result
     }, context))
 
     //
     // DELETE
     router.delete('/users/:id', baseRequestHandler(async ctx => {
-      await ctx.client.delete(`/users/${ctx.req.params.id}`)
-      const usersService = new UsersService({ schema: ctx.req.schema, accountability: ctx.req.accountability })
+      try {
+        await isRequestAllowed(ctx)
+      } catch (err) {
+        return handleErrorResponse(ctx, err)
+      }
+
+      const {client, req, res} = ctx
+      await client.delete(`/users/${ctx.req.params.id}`)
+      const usersService = new UsersService({schema: req.schema, accountability: req.accountability})
       try {
         const user = (await usersService.readByQuery({
           filter: {
-            external_identifier: ctx.req.params.id
+            external_identifier: req.params.id
           }
         })).shift()
         if (user) {
           await usersService.deleteOne(user.id)
         }
-        logger.info(`${logPrefix}Deleted Directus user with ID ${user.id}`)
+        logger.info(`${LOG_PREFIX}Deleted Directus user with ID ${user.id}`)
         return {
-          id: ctx.req.params.id,
+          id: req.params.id,
           directusId: user?.id
         }
       } catch (err) {
-        logger.error(`${logPrefix}Failed to delete Directus user: ${err.message}`)
+        logger.error(`${LOG_PREFIX}Failed to delete Directus user: ${err.message}`)
         return {
-          id: ctx.req.params.id,
+          id: req.params.id,
           directusError: err.message
         }
       }
@@ -211,8 +254,16 @@ export default {
     //
     // Reset password
     router.post('/users/:id/password', baseRequestHandler(async ctx => {
+      const {client, req, res} = ctx
+
+      try {
+        await isRequestAllowed(ctx)
+      } catch (err) {
+        return handleErrorResponse(ctx, err)
+      }
+
       const temporaryPassword = generatePassword(OTP_LENGTH)
-      await ctx.client.put(`/users/${ctx.req.params.id}/reset-password`, {
+      await client.put(`/users/${req.params.id}/reset-password`, {
         value: temporaryPassword,
         temporary: true
       })
@@ -224,21 +275,21 @@ export default {
     //
     // Associations
     router.get('/associations', baseRequestHandler(async ctx => {
-      const { data } = await ctx.client.get('/groups')
+      const {data} = await ctx.client.get('/groups')
       return data.filter(group => enumAssociation.includes(group.name)).map(group => group.name)
     }, context))
 
     //
     // Types
     router.get('/types', baseRequestHandler(async ctx => {
-      const { data } = await ctx.client.get('/groups')
+      const {data} = await ctx.client.get('/groups')
       return data.filter(group => enumType.includes(group.name)).map(group => group.name)
     }, context))
 
     //
     // Profiles
     router.get('/profiles', baseRequestHandler(async ctx => {
-      const { data } = await ctx.client.get('/groups')
+      const {data} = await ctx.client.get('/groups')
       return data.filter(group => profilePattern.test(group.name)).map(group => group.name)
     }, context))
   }
